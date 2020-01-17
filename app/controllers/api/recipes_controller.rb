bb8c7e04b7dea7851192d2ae5cf8ae28f67d8378
“jamalnuman” <jnuman82@gmail.com>
2020-01-17 09:40:41 -0600

    first push of the day

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,9 +1,16 @@
 class Api::RecipesController < ApplicationController
-  def one_recipe_action
-    @recipe = Recipe.first
-    render "one_recipe_view.json.jb"
+ 
+ def index 
+  @recipes = Recipe.all 
+  render 'index.json.jb'
+end
 
-  end
+def show
+  @recipe = Recipe.find(params[:id])
+  render 'show.json.jb'
+end
+
+
 
 
 
